86d11be5ffb4e6e55247288a123295369f8c405d
Vinicius <vinicius.benfica.ruy@gmail.com>
2023-12-12 11:21:16 -0300

    add routes

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -5,9 +5,12 @@ import (
 	"encoding/json"
 	"mensageria/internal/infra/akafka"
 	"mensageria/internal/infra/repository"
+	"mensageria/internal/infra/web"
 	"mensageria/internal/usecase"
+	"net/http"
 
 	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
@@ -18,15 +21,27 @@ func main() {
 
 	defer db.Close()
 
-	msgChan := make(chan *kafka.Message)
-	go akafka.Consume([]string{"products"}, "host.docker.internal9094", msgChan)
-
 	repository := repository.NewProductRepository(db)
 	createProductUsecase := usecase.NewCreateProductRepository(repository)
+	listProductUseCase := usecase.NewListProductRepository(repository)
+
+	productHandlers := web.NewProductHandlers(createProductUsecase, listProductUseCase)
+
+	r := chi.NewRouter()
+	r.Post("/products", productHandlers.CreateProductHandler)
+	r.Get("/products", productHandlers.ListProducsHandler)
+
+	go http.ListenAndServe(":8000", r)
+
+	msgChan := make(chan *kafka.Message)
+	go akafka.Consume([]string{"products"}, "host.docker.internal9094", msgChan)
 
 	for msg := range msgChan {
 		dto := usecase.CreateProductInputDto{}
 		err := json.Unmarshal(msg.Value, &dto)
+		if err != nil {
+
+		}
 
 		_, err = createProductUsecase.Execute(dto)
 	}
